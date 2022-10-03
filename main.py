5a3140b74272a2df0cbc92c4f52d19686b03fbb4
eusaha <114933587+eusaha@users.noreply.github.com>
2022-10-03 20:11:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import telebot
+
+
+bot = telebot.TeleBot("5623165505:AAEgJEFk0BQK82qwvqDXfxcs8ast57q5NCk")
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+  bot.reply_to(message, "Hello! I'm Uvindu Bro Chat Bot")
+
+
+@bot.message_handler(commands=["how"])
+def send_message(message):
+  bot.send_message(message.chat.id, "https://youtube.com/c/Uvindubro")
+
+
+
+bot.polling()
