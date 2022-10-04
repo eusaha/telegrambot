21e333861242b3eee9204a2b1f914a4be769e7de
eusaha <114933587+eusaha@users.noreply.github.com>
2022-10-04 19:04:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import telebot
 bot = telebot.TeleBot("5623165505:AAEgJEFk0BQK82qwvqDXfxcs8ast57q5NCk")
 
 
-@bot.message_handler(commands=["ai"])
-def send_welcome(message):
-  bot.reply_to(message, " ඇයි පුකද බලන්නෙ")
+##@bot.message_handler(commands=["ai"])
+##def send_welcome(message):
+ ## bot.reply_to(message, " ඇයි පුකද බලන්නෙ")
 
+  def hi(update ,context):
+update.message.reply_text("ai ")
 
-@bot.message_handler(commands=["ai"])
-def send_message(message):
-  bot.send_message(message.chat.id, "https://youtube.com/c/Uvindubro")
 
 
 
