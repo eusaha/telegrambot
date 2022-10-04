eebd72081aef56d5d6bb2fd76d0a55e506c2b44e
eusaha <114933587+eusaha@users.noreply.github.com>
2022-10-04 18:35:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import telebot
 bot = telebot.TeleBot("5623165505:AAEgJEFk0BQK82qwvqDXfxcs8ast57q5NCk")
 
 
-@bot.message_handler(commands=["a"])
+@bot.message_handler(commands=["ai"])
 def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm Uvindu Bro Chat Bot")
+  bot.reply_to(message, " ඇයි පුකද බලන්නෙ")
 
 
-@bot.message_handler(commands=["how"])
+@bot.message_handler(commands=["ai"])
 def send_message(message):
   bot.send_message(message.chat.id, "https://youtube.com/c/Uvindubro")
 
