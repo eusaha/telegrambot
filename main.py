53e6cb9fcb6bfe6b87f6b39b446eb3a15eb6a03d
eusaha <114933587+eusaha@users.noreply.github.com>
2022-10-04 18:30:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import os
 import telebot
 
 
-bot = telebot.TeleBot(' 5623165505:AAEgJEFk0BQK82qwvqDXfxcs8ast57q5NCk ')
+bot = telebot.TeleBot("5623165505:AAEgJEFk0BQK82qwvqDXfxcs8ast57q5NCk")
 
 
-@bot.message_handler(commands=["hi"])
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm Chat Bot")
-  
-  
+  bot.reply_to(message, "Hello! I'm Uvindu Bro Chat Bot")
+
+
+@bot.message_handler(commands=["how"])
+def send_message(message):
+  bot.send_message(message.chat.id, "https://youtube.com/c/Uvindubro")
+
 
 
 bot.polling()
