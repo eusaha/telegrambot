a26e277b4063234c0e9bfbfa46b66ec3d6cde5a4
eusaha <114933587+eusaha@users.noreply.github.com>
2022-10-04 19:19:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ bot = telebot.TeleBot("5623165505:AAEgJEFk0BQK82qwvqDXfxcs8ast57q5NCk")
 @bot.message_handler(commands=["ai"])
 def send_message(message):
  bot.reply_to(message, " ඇයි පුකද බලන්නෙ")
+ 
+ @bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
 
 
 
